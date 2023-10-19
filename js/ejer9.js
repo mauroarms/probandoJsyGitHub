9a3092d533b8b8b7effe0066641168a3e063af40
mauroarms <mauro.armas14@gmail.com>
2023-10-19 19:10:05 -0300

    Cree los archivos e hice el index

---
+++ js/ejer9.js
@@ -0,0 +1,55 @@
+/*9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+Ejemplo:
+
+
+
+input: Hola mundo
+Output: oauo*/
+
+let frase = prompt("Escriba la frase:")
+let longitud = frase.length
+let puntero = 0
+let fraseVocales = ""
+
+// switch(frase.charAt(puntero)){
+
+//     case 'a':
+//         fraseVocales += "a"
+//         puntero++    
+//      break;
+//     case 'e':
+//         fraseVocales += "e"
+//         puntero++  
+//      break;
+//     case 'i':
+//         fraseVocales += "i"
+//         puntero++  
+//      break;
+//     case 'o':
+//         fraseVocales += "o"
+//         puntero++  
+//      break;
+//     case 'u':
+//         fraseVocales += "u"
+//         puntero++  
+//      break;
+//     default:
+//         puntero++ 
+        
+// }
+
+
+
+// if( == 'e'){
+
+// }
+// if( == 'i'){
+
+// }
+// if( == 'o'){
+
+// }
+// if( == 'u'){
+
+// }
